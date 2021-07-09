210c20f48f9102716da828ad95540e0b15f2ee70
saptarshidas131 <saptarshidas131@gmail.com>
2021-07-09 21:25:46 +0530

    added ex6.py

---
+++ ex6.py
@@ -0,0 +1,37 @@
+# assign types_of_people the value 10
+types_of_people = 10
+# assign the string to x
+x = f"There are {types_of_people} types of people."
+
+# assign binary the string binary
+binary = "binary"
+# assign do_not the string don't
+do_not = "don't"
+# assign y the string
+y = f"Those who know {binary} and those who {do_not}."
+
+# print x
+print(x)
+# print y
+print(y)
+
+# print I said and value of x
+print(f"I said: {x}")
+# print I also said: value of y
+print(f"I also said: '{y}'")
+
+# set hilarious to False
+hilarious = False
+# assign the string to joke_evaluation
+joke_evaluation = "Isn't that joke so funny?! {}"
+
+# print value  of joke_evaluation and the value of hilarious
+print(joke_evaluation.format(hilarious))
+
+# set w to the string
+w = "This is the left side of ..."
+# set e to the string
+e = "a string with a right side."
+
+# concatenate w and e and print
+print(w + e)
